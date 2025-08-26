b522fd8ade951b976c37a34c1b26655792d2c937
Nicholas DeMarinis <ndemarinis@wpi.edu>
2025-08-26 10:56:52 -0400

    Midterm

--- src/siteloader.php
+++ src/siteloader.php
@@ -18,6 +18,7 @@ class SiteLoader {
         "Collator" => "lib/collatorshim.php",
         "ContainerServiceClient" => "src/container_service.php",
         "JobRequest" => "src/container_service.php",
+        "Midterm2022" => "/opt/peteramati/conf/cs300-s22/midterm2022.php"
     ];
 
     static $suffix_map = [
